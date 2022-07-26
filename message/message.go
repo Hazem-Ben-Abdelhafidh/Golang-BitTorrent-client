884406f467b2f97f15fab581e48b4cce9972cd34
Hazem-Ben-Abdelhafidh <morbidvisions@yahoo.com>
2022-07-26 17:05:40 +0100

    serializing message

--- message/message.go
+++ message/message.go
@@ -1,5 +1,7 @@
 package message
 
+import "encoding/binary"
+
 type messageID uint8
 
 const (
@@ -13,3 +15,29 @@ const (
 	MsgPiece         messageID = 7
 	MsgCancel        messageID = 8
 )
+
+type Message struct {
+	ID      messageID
+	Payload []byte
+}
+
+// Serialize serializes a message into a buffer of the form
+// <length prefix><message ID><payload>
+// Interprets `nil` as a keep-alive message
+
+func (m *Message) Serialize() []byte {
+	if m == nil {
+		return make([]byte, 4)
+	}
+	buff := make([]byte, 1)
+	buff[0] = 4
+	buff[1] = byte(m.ID)
+	copy(buff[2:], m.Payload)
+
+	length := uint32(len(m.Payload) + 1) // +1 for id
+	buf := make([]byte, 4+length)
+	binary.BigEndian.PutUint32(buf[0:4], length)
+	buf[4] = byte(m.ID)
+	copy(buf[5:], m.Payload)
+	return buf
+}
